edd853b50e214d60a5d6bb844b9eb1e11479a53d
mattw0204 <45386400+mattw0204@users.noreply.github.com>
2021-12-13 12:30:26 +0000

    Lesson1

--- js/TankScene.js
+++ js/TankScene.js
@@ -1,9 +1,21 @@
 class TankScene extends Phaser.Scene {
+    
+    /** @type {Phaser.Tilemaps.Tilemap} */
+    map
+    /** @type {Phaser.Tilemaps.TilemapLayer} */
+    destructLayer
     preload() {
-
+        this.load.image('tileset', 'assets/tanks/landscape-tileset.png')
+        this.load.tilemapTiledJSON('level1', 'assets/level1.json')
     }
     create() {
-
+        this.map = this.make.tilemap({key:'level1'})
+        const landscape = this.map.addTilesetImage('landscape-tileset','tileset')
+        this.map.createLayer('groundLayer',[landscape], 0, 0)
+        this.destructLayer = this.map.createLayer('destructableLayer',[landscape],0,0)
+        this.destructLayer.setCollisionByProperty({collides:true})
+        this.cameras.main.setBounds(0,0,this.map.widthInPixels,this.map.heightInPixels)
+        this.physics.world.setBounds(0,0, this.map.widthInPixels,this.map.heightInPixels)
     }
     update(time, delta) {
 
